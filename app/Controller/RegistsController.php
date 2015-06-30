a2c14457b06652479597651a0dec7e7b2e3f8449
sttr110 <mydogs@live.jp>
2015-06-30 17:44:11 +0000

    コントローラの作成

---
+++ app/Controller/RegistsController.php
@@ -0,0 +1,12 @@
+<?php
+
+class RegistsController extends AppController {
+  public $helpers = array('Html', 'Form');
+
+  public function index() {
+    $this->set('words', $this->Regist->find('all');
+
+  }
+
+}
+?>
