da17f45d242a2e47c72c1bc427bb8e1cddfe698d
sttr110 <mydogs@live.jp>
2015-07-02 12:47:26 +0000

    キーボードっぽくデザイン、keyを話した時に色を元に戻す

--- app/webroot/js/function.js
+++ app/webroot/js/function.js
@@ -1,9 +1,16 @@
 $(function() {
   document.onkeydown = type_key;
 
-  function type_key () {
+  function type_key() {
     if(event.keyCode == 70) {
-      $("#key_f").css("color", "#fc8"); 
+      $("#key_f").css("background", "#fc8"); 
     }
   }
+  document.onkeyup = type_key_up; 
+  function type_key_up() {
+    if(event.keyCode == 70) {
+      $("#key_f").css("background", "#fff"); 
+    }
+  }
+ 
 });
